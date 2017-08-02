2b47f3d27985af3d0c047989e8b2d6dad6a1484a
avi wilensky <avi@promediacorp.com>
2017-08-01 22:10:00 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # react-lex
+
 ReactJS AWS Lex Chat Component
+
+## Screenshot
+<img src="http://i.imgur.com/jaTxe6Y.png" width="50%">
+
+## Prerequisites
+The AWS Javascript SDK for the browser is required. This package will install the entire SDK, but all you need is AWS.LexRuntime and AWS.CognitoIdentity. We recommend you build your own version of the AWS SDK for JavaScript using the [builder](https://sdk.amazonaws.com/builder/js/) to include only the services you need.
+
+You will need to set up an AWS Cognito federated identity pool, and pass the IdentityPoolId as props to the component. Be sure to enable access to unauthenticated identities, and modify the IAM roles to allow access to Amazon Lex. From the IAM console, attach the AmazonLexRunBotsOnly and AmazonPollyReadOnlyAccess policies. [This blog post](https://aws.amazon.com/blogs/ai/greetings-visitor-engage-your-web-users-with-amazon-lex/) walks through the process in detail. 
+
+
+
+## Installing the react-lex
+
+```
+npm install --save react-lex
+```
+
+## Using the Component
+
+
+Example:
+
+```js
+import LexChat from "./LexChat";
+
+class App extends Component {
+  render() {
+        <LexChat botName="OrderFlowers"
+                 IdentityPoolId="us-east-1:7292b8c0-56f1-4441-b2a6-xxxxxxxxxxxx"
+                 placeholder="Placeholder text"
+                 style={{position: 'absolute'}}
+                 backgroundColor="#FFFFFF"
+                 height="430px"
+                 headerText="Chat with our awesome bot" />
+  }
+}
+export default App;
+```
+* Your botname (ie. "OrderFlowers") is a required prop.
+* Your IdentityPoolId is a required prop.
+
+##License
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Sabin Bir Bajracharya.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/index.js
@@ -0,0 +1,198 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+import 'aws-sdk';
+import "./styles/chatbot.css";
+
+
+class LexChat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '', 
+      lexUserId: 'chatbot-demo' + Date.now(), 
+      sessionAttributes: {}, visible: 'closed'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementById("inputField").focus();
+    AWS.config.region = 'us-east-1';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: this.props.IdentityPoolId,
+    });
+    var lexruntime = new AWS.LexRuntime();
+    this.lexruntime = lexruntime;
+
+  }
+
+  handleClick() {
+    this.setState({ visible: this.state.visible == 'open'? 'closed' : 'open' });
+    console.log(this.state);
+  }
+
+  pushChat(event) {
+    event.preventDefault();
+
+    var inputFieldText = document.getElementById('inputField');
+
+    if (inputFieldText && inputFieldText.value && inputFieldText.value.trim().length > 0) {
+
+      // disable input to show we're sending it
+      var inputField = inputFieldText.value.trim();
+      inputFieldText.value = '...';
+      inputFieldText.locked = true;
+
+      // send it to the Lex runtime
+      var params = {
+        botAlias: '$LATEST',
+        botName: this.props.botName,
+        inputText: inputField,
+        userId: this.state.lexUserId,
+        sessionAttributes: this.state.sessionAttributes
+      };
+      this.showRequest(inputField);
+      var a = function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+          this.showError('Error:  ' + err.message + ' (see console for details)')
+        }
+        if (data) {
+          // capture the sessionAttributes for the next cycle
+          this.setState({sessionAttributes: data.sessionAttributes})
+          //sessionAttributes = data.sessionAttributes;
+          // show response and/or error/dialog status
+          this.showResponse(data);
+        }
+        // re-enable input
+        inputFieldText.value = '';
+        inputFieldText.locked = false;
+      };
+
+      this.lexruntime.postText(params, a.bind(this));
+    }
+    // we always cancel form submission
+    return false;
+  }
+
+  showRequest(daText) {
+    var conversationDiv = document.getElementById('conversation');
+    var requestPara = document.createElement("P");
+    requestPara.className = 'userRequest';
+    requestPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(requestPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+  }
+
+  showError(daText) {
+
+    var conversationDiv = document.getElementById('conversation');
+    var errorPara = document.createElement("P");
+    errorPara.className = 'lexError';
+    errorPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(errorPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+  }
+
+  showResponse(lexResponse) {
+
+    var conversationDiv = document.getElementById('conversation');
+    var responsePara = document.createElement("P");
+    responsePara.className = 'lexResponse';
+    if (lexResponse.message) {
+      responsePara.appendChild(document.createTextNode(lexResponse.message));
+      responsePara.appendChild(document.createElement('br'));
+    }
+    if (lexResponse.dialogState === 'ReadyForFulfillment') {
+      responsePara.appendChild(document.createTextNode(
+        'Ready for fulfillment'));
+      // TODO:  show slot values
+    } else {
+      responsePara.appendChild(document.createTextNode(
+        ''));
+    }
+    conversationDiv.appendChild(responsePara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({data: event.target.value});
+  }
+
+  render() {
+
+    const inputStyle = {
+      padding: '4px',
+      fontSize: 24,
+      width: '394px',
+      borderRadius: '1px',
+      border: '10px'
+    }
+
+    const conversationStyle = {
+      width: '400px',
+      height: this.props.height,
+      border: 'px solid #ccc',
+      backgroundColor: this.props.backgroundColor,
+      padding: '4px',
+      overflow: 'scroll',
+      borderBottom: 'thin ridge #bfbfbf'
+    }
+
+    const headerRectStyle = {
+      backgroundColor: '#000000', 
+      width: '400px', 
+      height: '40px',
+      textAlign: 'center',
+      paddingTop: 12,
+      paddingBottom: -12,
+      color: '#FFFFFF',
+      fontSize: '24px'
+    }
+
+    const chatcontainerStyle = {
+      backgroundColor: '#FFFFFF',
+    }
+
+    const chatFormStyle = {
+      margin: '1px', 
+      padding: '2px'
+    }
+
+
+    return (
+      <div id="chatwrapper">
+        <div id="chat-header-rect" style={headerRectStyle} onClick={this.handleClick} >{this.props.headerText}
+              {(this.state.visible === 'open') ? <span className='chevron top'></span> : <span className='chevron bottom'></span>}
+        </div>
+        <div id="chatcontainer" className={this.state.visible} style={chatcontainerStyle}>
+          <div id="conversation" style={conversationStyle} ></div>
+            <form id="chatform" style={chatFormStyle} onSubmit={this.pushChat.bind(this)}>
+                <input type="text"
+                       id="inputField"
+                       size="40"
+                       value={this.state.data}
+                       placeholder={this.props.placeholder}
+                       onChange={this.handleChange.bind(this)}
+                       style={inputStyle}
+                      />
+            </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+LexChat.propTypes = {
+  botName: PropTypes.string,
+  IdentityPoolId: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string,
+  height: PropTypes.number,
+  headerText: PropTypes.string
+}
+
+export default LexChat;
+
